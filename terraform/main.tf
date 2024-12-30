bf346ad96139bbafd7f84d43ee751808aa271744
Ezekiel Umesi <obusorezekiel@gmail.com>
2024-12-30 16:58:32 +0100

    Feat: infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,11 +25,11 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.31.0"
 
-  cluster_name                   = local.name
-  cluster_version                = "1.30"
-  cluster_endpoint_public_access = true
+  cluster_name                             = local.name
+  cluster_version                          = "1.30"
+  cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
-  enable_irsa = true
+  enable_irsa                              = true
 
   cluster_addons = {
     coredns = {
@@ -50,7 +50,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    instance_types = ["t2.medium", "t2.micro"]
 
     attach_cluster_primary_security_group = true
   }
@@ -79,10 +79,11 @@ resource "null_resource" "ansible_provisioner" {
 
   provisioner "local-exec" {
     command = <<-EOT
+      aws eks update-kubeconfig --name ${local.name} --region us-east-1 && \
       ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-        -i inventory.yml \
+        -i ../ansible/inventory.yml \
         --extra-vars "cluster_name=${local.name}" \
-        site.yml
+        ../ansible/site.yml
     EOT
   }
 }
